ce8e3edd7fadf8e93be40ec48a1311d2b3e3ef29
Ivan Ying <yyjazsf@live.cn>
2016-10-24 17:28:56 +0800

    使用和 java.math.BigInteger 一样的方法名称

---
+++ src/divide.ts
@@ -0,0 +1,6 @@
+/**
+ * 
+ */
+export default function divide(num1: Array<number>, num2: Array<number>) {
+  return 'todo';
+};
---
+++ src/multiply.ts
@@ -0,0 +1,6 @@
+/**
+ * 
+ */
+export default function multiply(num1: Array<number>, num2: Array<number>) {
+  return 'todo';
+};
